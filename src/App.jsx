9df55e98ceaf378f0d753f6da9868d513928a130
erfan <mahdi.farahmandsafa@gmail.com>
2025-02-28 17:07:09 +0330

    create project layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Grid, GridItem, Button } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Grid
+      m="5px"
+      p="5px"
+      gap="4px"
+      bg="gray.200"
+      templateRows="repeat(7, 1fr)"
+      minHeight="100vh"
+    >
+      <GridItem bg="green.400" rowSpan={1}>
+        <Navbar />
+      </GridItem>
 
-export default App
+      <GridItem bg="blue.400" rowSpan={6}>
+        <Button colorPalette="teal">theme</Button>
+      </GridItem>
+    </Grid>
+  );
+}
