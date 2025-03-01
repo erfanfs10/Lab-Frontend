61d9fee3dd50fe55b27bfa8951187388d1d25824
erfan <mahdi.farahmandsafa@gmail.com>
2025-03-01 20:17:20 +0330

    change navbar elements position by current languege: rtl

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import {
   Flex,
   HStack,
-  Button,
   Heading,
   Spacer,
   LocaleProvider,
+  Separator
 } from "@chakra-ui/react";
 import ColorModeButton from "./ColorMode";
 import SelectLanguage from "./SelectLanguage";
@@ -14,15 +14,18 @@ export default function Navbar() {
   const [lang, setLang] = useState("eng");
 
   return (
+    <>
     <LocaleProvider locale="ar-Ar">
-      <Flex as="nav" m="20px" dir={lang==="fa"?"rtl":""} >
-        <Heading>The Image Converter App</Heading>
+      <Flex as="nav" m="10px" dir={lang==="fa"?"rtl":""} >
+        <Heading ml="20px">{lang==="fa"? "برنامه کانورت عکس":"The Image Converter App"}</Heading>
         <Spacer />
-        <HStack spaceX="20px">
+        <HStack mr="20px" spaceX="20px">
           <SelectLanguage onSetLang={(val)=>setLang(val)} />
           <ColorModeButton />
         </HStack>
       </Flex>
     </LocaleProvider>
+    <Separator size="md"/>
+    </>
   );
 }
