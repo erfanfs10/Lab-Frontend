19ff5946a573f3a01363558b708f68a41b863627
erfan <mahdi.farahmandsafa@gmail.com>
2025-03-02 21:00:36 +0330

    add websocket status in app - change rowSpan and templateRows

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import {useState} from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Convert from "./components/Convert";
 
 export default function App() {
+    const [wsStatus, setWsStatus] = useState(false);
+  
   return (
     <Grid
       p="10px"
       gap="4px"
-      templateRows="repeat(7, 1fr)"
+      templateRows="repeat(10, 1fr)"
       minHeight="100vh"
     >
       <GridItem rowSpan={1}>
-        <Navbar />
+        <Navbar wsStatus={wsStatus} />
       </GridItem>
 
-      <GridItem rowSpan={6}>
-        <Convert/>
+      <GridItem rowSpan={9}>
+        <Convert wsStatus={wsStatus} setWsStatus={setWsStatus}/>
       </GridItem>
     </Grid>
   );
