d57ce45629176fdfd4ee5769271032b61f149cd4
erfan <mahdi.farahmandsafa@gmail.com>
2025-03-16 14:33:19 +0330

    add websocket status useState in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Convert from "./components/Convert";
 
 export default function App() {
-    const [wsStatus, setWsStatus] = useState(false);
-  
+  const [wsStatus, setWsStatus] = useState(false);
+
   return (
-    <Grid
-      p="10px"
-      gap="4px"
-      templateRows="repeat(10, 1fr)"
-      minHeight="100vh"
-    >
+    <Grid p="10px" gap="4px" templateRows="repeat(10, 1fr)" minHeight="100vh">
       <GridItem rowSpan={1}>
         <Navbar wsStatus={wsStatus} />
       </GridItem>
 
       <GridItem rowSpan={9}>
-        <Convert wsStatus={wsStatus} setWsStatus={setWsStatus}/>
+        <Convert wsStatus={wsStatus} setWsStatus={setWsStatus} />
       </GridItem>
     </Grid>
   );
