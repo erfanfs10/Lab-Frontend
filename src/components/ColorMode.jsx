b396118d0e4d67a06c34d7b3b77ecadd1c796c46
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-10 21:39:03 +0330

    add hideBelow breakpoint

--- src/components/ColorMode.jsx
+++ src/components/ColorMode.jsx
@@ -1,12 +1,24 @@
-import { Button } from "@chakra-ui/react"
-import { useColorMode } from "./ui/color-mode"
+import { Button, Box } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
 import { VscColorMode } from "react-icons/vsc";
 
-export default function ColorModeButton () {
-  const { toggleColorMode } = useColorMode()
+export default function ColorModeButton({ hideBelow }) {
+  const { toggleColorMode } = useColorMode();
   return (
-    <Button variant="outline" onClick={toggleColorMode}>
-      <VscColorMode/>
-    </Button>
-  )
+    <Box hideBelow={hideBelow}>
+      <Button
+        size={{
+          base: "sm",
+          sm: "sm",
+          md: "md",
+          lg: "md",
+          xl: "md",
+        }}
+        variant="outline"
+        onClick={toggleColorMode}
+      >
+        <VscColorMode />
+      </Button>
+    </Box>
+  );
 }
