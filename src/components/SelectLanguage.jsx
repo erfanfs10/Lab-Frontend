b9e83b52cfe8758d9410969233417b9b43befc5a
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-10 21:42:18 +0330

    store lang in localStorage, add breakpoints

--- src/components/SelectLanguage.jsx
+++ src/components/SelectLanguage.jsx
@@ -1,15 +1,40 @@
 import { NativeSelect, HStack, Icon } from "@chakra-ui/react";
 import { IoLanguage } from "react-icons/io5";
 
-export default function SelectLanguage({onSetLang}) {
+export default function SelectLanguage({ setLang, lang, hideBelow }) {
   return (
     <>
-      <HStack>
-        <Icon fontSize="25px">
+      <HStack hideBelow={hideBelow}>
+        <Icon
+          size={{
+            base: "sm",
+            sm: "sm",
+            md: "md",
+            lg: "md",
+            xl: "md",
+          }}
+        >
           <IoLanguage />
         </Icon>
-        <NativeSelect.Root size="md">
-          <NativeSelect.Field onChange={(e) => onSetLang(e.currentTarget.value)}>
+        <NativeSelect.Root
+          size={{
+            base: "sm",
+            sm: "sm",
+            md: "md",
+            lg: "md",
+            xl: "md",
+          }}
+        >
+          <NativeSelect.Field
+            placeholder={lang === "eng" ? "Select Language" : "انتخاب زبان"}
+            onChange={(e) => {
+              if (e.currentTarget.value) {
+                setLang(e.currentTarget.value);
+                localStorage.setItem("lang", e.currentTarget.value);
+              }
+              return;
+            }}
+          >
             <option value="eng">English</option>
             <option value="fa">Farsi</option>
           </NativeSelect.Field>
